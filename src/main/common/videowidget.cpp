b989b2d47ce11a644918067a1867b73f2a25d64d
zhjun5337 <zhjun5337@163.com>
2014-11-11 20:27:23 +0800

    添加Qt5支持，但是在Qt5中视频播放会出现按钮显示为黑，有时候视频不是从头开始播放的。

--- src/main/common/videowidget.cpp
+++ src/main/common/videowidget.cpp
@@ -1,13 +1,15 @@
 #include "videowidget.h"
 #include "../../commom/staticbutton.h"
 #include <QLabel>
-
-
+#include <QDir>
+#include <QDebug>
 VideoWidget::VideoWidget(QWidget *parent) :
     QDialog(parent), m_isFirst(true)
 {
     this->setFixedSize(904, 600);
     this->setWindowFlags(Qt::FramelessWindowHint | Qt::Dialog);
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+
     m_mediaObject = new Phonon::MediaObject(this);//MediaObject 用于对媒体流基本控制，如播放，暂停，停止等，通过读取MediaSource
     connect(m_mediaObject, SIGNAL(finished()), this, SLOT(playFinished()));
     connect(m_mediaObject, SIGNAL(stateChanged(Phonon::State,Phonon::State)), \
@@ -20,7 +22,20 @@ VideoWidget::VideoWidget(QWidget *parent) :
     Phonon::createPath(m_mediaObject,m_videoWidget);
     m_mediaObject->setCurrentSource(Phonon::MediaSource("./resource/video/360SafeVideo.wmv"));
     m_videoWidget->setGeometry(rect());
+#else
+    m_oldPosition = 0;
+    QMediaPlaylist *playlist = new QMediaPlaylist;
+    playlist->addMedia(QUrl::fromLocalFile(QDir::currentPath() + "/resource/video/360SafeVideo.wmv"));
 
+    m_player = new QMediaPlayer;
+    m_player->setPlaylist(playlist);
+    connect(m_player, SIGNAL(durationChanged(qint64)), this, SLOT(durationChanged(qint64)));
+    connect(m_player, SIGNAL(positionChanged(qint64)), this, SLOT(positionChanged(qint64)));
+    QVideoWidget *videoWidget = new QVideoWidget(this);
+    videoWidget->setGeometry(rect());
+    m_player->setVideoOutput(videoWidget);
+    videoWidget->show();
+#endif
     m_close = new StaticButton(this);
     m_close->setOneButtonInfo(":/main/video_close");
     m_close->move(width()-m_close->width() - 5, 5);
@@ -38,7 +53,19 @@ VideoWidget::VideoWidget(QWidget *parent) :
 
 void VideoWidget::startVideo()
 {
+
+    m_endLabel->hide();
+    m_startLabel->show();
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+    m_isFirst = true;
     m_mediaObject->play();
+#else
+    m_oldPosition = 0;
+    m_player->stop();
+    m_player->setPosition(0);
+    m_player->play();
+#endif
+    m_close->repaint();
 }
 
 void VideoWidget::playFinished()
@@ -50,10 +77,17 @@ void VideoWidget::playFinished()
 
 void VideoWidget::closeWidget()
 {
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
     m_mediaObject->stop();
+#else
+    m_player->stop();
+#endif
+    m_endLabel->hide();
+    m_startLabel->show();
     this->close();
 }
 
+#if QT_VERSION < 0x050000
 void VideoWidget::playChanged(Phonon::State newstate, Phonon::State oldstate)
 {
     if(newstate == (Phonon::PlayingState) && m_isFirst)
@@ -63,3 +97,20 @@ void VideoWidget::playChanged(Phonon::State newstate, Phonon::State oldstate)
         update();
     }
 }
+#else
+void VideoWidget::durationChanged(qint64 duration)
+{
+    if(duration > 0)
+        m_duration = duration;
+}
+
+void VideoWidget::positionChanged(qint64 position)
+{
+    if(position > 0)
+        m_startLabel->hide();
+    if(m_oldPosition > 0 && position == 0)
+        this->playFinished();
+    m_oldPosition = position;
+}
+
+#endif
--- src/main/common/videowidget.h
+++ src/main/common/videowidget.h
@@ -3,11 +3,19 @@
 
 #include <QWidget>
 #include <QDialog>
-#include <phonon/mediasource.h>
-#include <phonon/mediaobject.h>
-#include <phonon/AudioOutput>
-#include <phonon/videoplayer.h>
-#include <phonon/VideoWidget>
+
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
+    #include <phonon/mediasource.h>
+    #include <phonon/mediaobject.h>
+    #include <phonon/AudioOutput>
+    #include <phonon/videoplayer.h>
+    #include <phonon/VideoWidget>
+#define USE_QT4 1
+#else
+    #include <QVideoWidget>
+    #include <QMediaPlayer>
+    #include <QMediaPlaylist>
+#endif
 
 class StaticButton;
 class QLabel;
@@ -21,11 +29,24 @@ public:
 private slots:
     void playFinished();
     void closeWidget();
+//只能使用这种形式在slot中判断版本，否则moc会报错
+#if QT_VERSION < 0x050000
     void playChanged(Phonon::State newstate, Phonon::State oldstate);
+#else
+    void durationChanged(qint64 duration);
+    void positionChanged(qint64 position);
+#endif
+
 private:
+#if (QT_VERSION < QT_VERSION_CHECK(5,0,0))
     Phonon::MediaObject *m_mediaObject;
     Phonon::VideoWidget *m_videoWidget;
     Phonon::AudioOutput *m_audioOutput;
+#else
+    QMediaPlayer    *m_player;
+    qint64  m_duration;
+    qint64  m_oldPosition;
+#endif
     StaticButton    *m_close;
     QLabel  *m_startLabel;
     QLabel  *m_endLabel;
